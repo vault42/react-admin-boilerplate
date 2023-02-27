d68d15f79cba7cfd98cc6bcb1a35c6d088d7d34c
42arch <rend42@163.com>
2023-02-27 14:57:31 +0800

    refactor: :art: update layout

--- src/layout/NavHeader.tsx
+++ src/layout/NavHeader.tsx
@@ -1,30 +1,50 @@
 import { FC } from 'react'
 import ColorControl from '@components/ColorControl'
-import { Group, useMantineColorScheme } from '@mantine/core'
+import {
+  Burger,
+  Group,
+  Header,
+  MediaQuery,
+  useMantineTheme
+} from '@mantine/core'
 import { useAppDispatch } from '@hooks/use-app-dispatch'
 import { useAppSelector } from '@hooks/use-app-selector'
 import { setPrimaryColor } from '@store/global-slice'
 import ToggleTheme from '../components/ToggleTheme'
-import { Logo } from './Logo'
+import { setNavOpen } from '@store/global-slice'
 
 const NavHeader: FC = () => {
   const dispatch = useAppDispatch()
-  const { colorScheme } = useMantineColorScheme()
+  const theme = useMantineTheme()
+  const { navOpen } = useAppSelector((state) => state.global)
   const { primaryColor } = useAppSelector((state) => state.global)
 
   return (
-    <div className='w-full flex justify-between'>
-      <Logo colorScheme={colorScheme} primaryColor={primaryColor} />
-      <Group>
-        <ColorControl
-          onChange={(color) => {
-            dispatch(setPrimaryColor(color))
-          }}
-          value={primaryColor}
-        />
-        <ToggleTheme />
-      </Group>
-    </div>
+    <Header height={{ base: 48, md: 70 }} p='md'>
+      <div className='flex items-center w-ful h-full'>
+        <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+          <Burger
+            opened={navOpen}
+            onClick={() => dispatch(setNavOpen(!navOpen))}
+            size='sm'
+            color={theme.colors.gray[6]}
+            mr='xl'
+          />
+        </MediaQuery>
+
+        <div className='w-full flex justify-end'>
+          <Group>
+            <ColorControl
+              onChange={(color) => {
+                dispatch(setPrimaryColor(color))
+              }}
+              value={primaryColor}
+            />
+            <ToggleTheme />
+          </Group>
+        </div>
+      </div>
+    </Header>
   )
 }
 
--- src/layout/NavSideBar.tsx
+++ src/layout/NavSideBar.tsx
@@ -1,25 +1,60 @@
 import { FC } from 'react'
 import { useAppSelector } from '@hooks/use-app-selector'
-import { Navbar, ScrollArea } from '@mantine/core'
+import {
+  Burger,
+  Code,
+  Group,
+  MediaQuery,
+  Navbar,
+  ScrollArea,
+  useMantineColorScheme,
+  useMantineTheme
+} from '@mantine/core'
+import { useAppDispatch } from '@hooks/use-app-dispatch'
+import { setNavOpen } from '@store/global-slice'
 import NavLinks from './NavLinks'
 import UserLink from './UserLink'
+import { Logo } from './Logo'
 
 const NavSideBar: FC = () => {
+  const dispatch = useAppDispatch()
   const { navOpen } = useAppSelector((state) => state.global)
+  const { colorScheme } = useMantineColorScheme()
+  const { primaryColor } = useAppSelector((state) => state.global)
+  const theme = useMantineTheme()
 
   return (
     <Navbar
-      p='md'
       hiddenBreakpoint='sm'
       hidden={!navOpen}
       width={{ sm: 250, lg: 300 }}
     >
-      <Navbar.Section grow component={ScrollArea}>
-        <div className='py-2'>{<NavLinks />}</div>
-      </Navbar.Section>
-      <Navbar.Section>
-        <UserLink />
-      </Navbar.Section>
+      <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+        <div className='h-[48px] p-4 flex items-center'>
+          <Burger
+            opened={navOpen}
+            onClick={() => dispatch(setNavOpen(!navOpen))}
+            size='sm'
+            color={theme.colors.gray[6]}
+          />
+        </div>
+      </MediaQuery>
+      <div className='h-full flex flex-col px-4 pb-4'>
+        <Navbar.Section px={8}>
+          <Group position='apart'>
+            <Logo colorScheme={colorScheme} primaryColor={primaryColor} />
+            <Code sx={{ fontWeight: 700 }}>v3.1.2</Code>
+          </Group>
+        </Navbar.Section>
+        <Navbar.Section grow component={ScrollArea}>
+          <div className='py-2'>
+            <NavLinks />
+          </div>
+        </Navbar.Section>
+        <Navbar.Section>
+          <UserLink />
+        </Navbar.Section>
+      </div>
     </Navbar>
   )
 }
